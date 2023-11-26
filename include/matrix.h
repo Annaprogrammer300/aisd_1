2bfcbca65e820067055978bfea6c022c10c49220
Annaprogrammer300 <star2004.as@gmail.com>
2023-11-27 01:42:26 +0400

    start1

--- include/matrix.h
+++ include/matrix.h
@@ -120,3 +120,191 @@ Matrix<T>::~Matrix() {
 	delete[] data_;
 	data_ = nullptr;
 }
+
+
+template <typename T>
+void Matrix<T>::Swap(Matrix<T>& other) noexcept {
+	std::swap(data_, other.data_);
+	std::swap(rows_, other.rows_);
+	std::swap(cols_, other.cols_);
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator=(Matrix<T> other) {
+	Swap(other);
+	return *this;
+}
+
+template <typename T>
+T& Matrix<T>::operator()(int row, int col) {
+	return data_[row][col];
+}
+
+template <typename T>
+const T& Matrix<T>::operator()(int row, int col) const {
+	return data_[row][col];
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator+=(const Matrix<T>& other) {
+	if (rows_ != other.rows_ || cols_ != other.cols_) {
+		throw std::invalid_argument("Matrix dimensions do not match");
+	}
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			data_[i][j] += other.data_[i][j];
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix<T>& other) const {
+	if (rows_ != other.rows_ || cols_ != other.cols_) {
+		throw std::invalid_argument("Matrix dimensions do not match");
+	}
+	Matrix<T> res = *this;
+	res += other;
+	return res;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-(const Matrix<T>& other) const {
+	if (rows_ != other.rows_ || cols_ != other.cols_) {
+		throw std::invalid_argument("Matrix dimensions do not match");
+	}
+	Matrix res(rows_, cols_, 0);
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			res(i, j) = data_[i][j] - other.data_[i][j];
+		}
+	}
+	return res;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T>& other) const {
+	if (cols_ != other.rows_) {
+		throw std::invalid_argument("Matrix dimensions do not match");
+	}
+	Matrix res(rows_, other.cols_, 0);
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < other.cols_; ++j) {
+			for (int k = 0; k < cols_; ++k) {
+				res(i, j) += data_[i][k] * other.data_[k][j];
+			}
+		}
+	}
+	return res;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(const T scalar) const {
+	Matrix<T> res(rows_, cols_, 0);
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			res(i, j) = data_[i][j] * scalar;
+		}
+	}
+	return res;
+}
+
+template <typename T>
+Matrix<double> Matrix<T>::operator/(const T scalar) const {
+	if (!scalar) {
+		throw std::invalid_argument("Divide by zero!");
+	}
+	Matrix<double> res(rows_, cols_, 0);
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			res(i, j) = static_cast<double>(data_[i][j]) / scalar;
+		}
+	}
+	return res;
+}
+
+template <typename T>
+int Matrix<T>::get_rows() const {
+	return rows_;
+}
+
+template <typename T>
+int Matrix<T>::get_cols() const {
+	return cols_;
+}
+
+template <typename T>
+bool operator==(const Matrix<T>& matrix_first, const Matrix<T>& matrix_second) {
+	if (matrix_first.get_rows() != matrix_second.get_rows() || matrix_first.get_cols() != matrix_second.get_cols()) {
+		return false;
+	}
+	const double kEpsilon = 1.0E-5;
+	for (int i = 0; i < matrix_first.get_rows(); ++i) {
+		for (int j = 0; j < matrix_first.get_cols(); ++j) {
+			if (matrix_first(i, j) - matrix_second(i, j) > kEpsilon)
+				return false;
+		}
+	}
+	return true;
+}
+
+template <>
+bool operator==(const Matrix<std::complex<double>>& matrix_first, const Matrix<std::complex<double>>& matrix_second) {
+	if (matrix_first.get_rows() != matrix_second.get_rows() || matrix_first.get_cols() != matrix_second.get_cols()) {
+		return false;
+	}
+	const double kEpsilon = 1.0E-5;
+	for (int i = 0; i < matrix_first.get_rows(); ++i) {
+		for (int j = 0; j < matrix_first.get_cols(); ++j) {
+			if ((abs(matrix_first(i, j)) - abs(matrix_second(i, j))) > kEpsilon)
+				return false;
+		}
+	}
+	return true;
+}
+
+template <typename T>
+bool operator!=(const Matrix<T>& matrix_first, const Matrix<T>& matrix_second) {
+	return (!(matrix_first == matrix_second));
+}
+
+template <>
+bool operator!=(const Matrix<std::complex<double>>& matrix_first, const Matrix<std::complex<double>>& matrix_second) {
+	return (!(matrix_first == matrix_second));
+}
+
+template <typename T>
+T Matrix<T>::Trace() const {
+	if (rows_ != cols_)
+		throw std::invalid_argument("Matrix is not square");
+	T res = 0;
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			if (i == j) res += data_[i][j];
+		}
+	}
+	return res;
+}
+
+template <>
+std::complex<double> Matrix<std::complex<double>>::Trace() const {
+	if (rows_ != cols_)
+		throw std::invalid_argument("Matrix is not square");
+	std::complex<double> res = 0;
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j)
+			if (i == j) res += data_[i][j];
+	}
+	return res;
+}
+
+template <typename T>
+void Matrix<T>::Print() const {
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			std::cout.precision(4);
+			std::cout << std::setw(5) << data_[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,15 @@
-#include "C:\Users\user\Desktop\Aisd_1\aisd_1\matrix.h"
+#include "C:\Users\user\Desktop\Aisd_1\aisd_1\include\matrix.h"
 
 
 
 
 int main()
 { // Пример использования
-    
+
+    Matrix<int> mat(3, 3, 1, 5);
+
+    std::cout << "Matrix A:\n";
+    mat.Print();
 
   
 
